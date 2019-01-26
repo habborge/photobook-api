82caa8b02be905b3f4d221a489bb85a6e0cca7cb
Gustavo Morales <gustavo.morales@gmail.com>
2019-01-26 08:49:57 -0500

    Add logging system with Winston and Morgan

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const addRequestId = require('express-request-id')();
 
+const logger = require('./config/logger');
 const api = require('./api/v1');
 
 const app = express();
 
+// setup middleware
+app.use(addRequestId);
+app.use(logger.requests);
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
@@ -16,15 +21,23 @@ app.use('/api/v1', api);
 
 // No route found handler
 app.use((req, res, next) => {
-  res.status(404);
-  res.json({
-    message: 'Error. Route not found',
+  const message = 'Route not found';
+  const statusCode = 404;
+
+  next({
+    message,
+    statusCode,
+    type: 'info',
   });
 });
 
 // Error handler
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
+  const { statusCode = 500, message, type = 'error' } = err;
+
+  const log = `${logger.header(req)} ${statusCode} ${message}`;
+
+  logger[type](log);
 
   res.status(statusCode);
   res.json({
